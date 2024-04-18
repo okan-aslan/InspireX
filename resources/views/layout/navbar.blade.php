941dca031e9770ca4768d4cb10ba22660e523c7e
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 15:42:36 +0300

    added login & register routes

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -4,9 +4,9 @@
             <a href="/" class="text-white font-bold text-lg">{{config('app.name')}}</a>
         </div>
         <div class="flex items-center space-x-4">
-            <button class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Login</button>
-            <button class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Register</button>
-            <button class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Profile</button>
+            <a href="{{route('login')}}"  class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Login</a>
+            <a href="{{route('register')}}"  class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Register</a>
+            <a href="#"  class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Profile</a>
         </div>
     </div>
 </nav>
