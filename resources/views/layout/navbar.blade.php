cb48470c0ca7372c1dbeda184d8b49164f7a080c
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 16:43:32 +0300

    edited navbar to auth

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,12 +1,25 @@
 <nav class="bg-blue-500 p-4">
     <div class="container mx-auto flex justify-between items-center">
         <div>
-            <a href="/" class="text-white font-bold text-lg">{{config('app.name')}}</a>
+            <a href="/" class="text-white font-bold text-lg">{{ config('app.name') }}</a>
         </div>
         <div class="flex items-center space-x-4">
-            <a href="{{route('login')}}"  class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Login</a>
-            <a href="{{route('register')}}"  class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Register</a>
-            <a href="#"  class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Profile</a>
+            @auth
+                <a href="#"
+                    class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">{{Auth::user()->name}}</a>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <a href=""
+                        class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Logout</a>
+                </form>
+            @endauth
+            @guest
+                <a href="{{ route('login') }}"
+                    class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Login</a>
+                <a href="{{ route('register') }}"
+                    class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Register</a>
+            @endguest
+
         </div>
     </div>
 </nav>
