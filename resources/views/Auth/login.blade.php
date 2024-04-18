a32f15cb9cc21674903069091f4c24f576c88d1f
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 16:43:47 +0300

    fixed bugs

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,14 +1,16 @@
 @extends('layout.app')
 
 @section('content')
-    <form>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                 Email Address
             </label>
             <input
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email" type="email" placeholder="Email Address">
+                id="email" type="email" placeholder="Email Address" name="email" required autocomplete="email"
+                autofocus>
         </div>
         <div class="mb-6">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
@@ -16,15 +18,16 @@
             </label>
             <input
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password" type="password" placeholder="Password">
+                id="password" type="password" placeholder="Password" name="password" required
+                autocomplete="current-password">
         </div>
         <div class="flex items-center justify-between">
             <button
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button">
+                type="submit">
                 Sign In
             </button>
-            <a href="{{route('register')}}"
+            <a href="{{ route('register') }}"
                 class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">Register</a>
         </div>
     </form>
