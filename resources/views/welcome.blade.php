2084c8cfaac5fd219762906c015d074a8ee356ad
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 21:26:16 +0300

    updated index view for not authenticated users

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,14 @@
 @extends('layout.app')
 
 @section('content')
-    @include('Components.tweet')
-
-    @include('Components.comment-write')
-
-    @include('Components.comment')
+    <div class="text-center">
+        <h2 class="text-2xl font-semibold mb-4">Welcome to Twitter Clone!</h2>
+        <p class="text-lg mb-4">Login or register to start sharing your thoughts with the world.</p>
+        <div class="flex justify-center">
+            <a href="{{ route('login') }}"
+                class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full mr-4">Login</a>
+            <a href="{{ route('register') }}"
+                class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-full">Register</a>
+        </div>
+    </div>
 @endsection
