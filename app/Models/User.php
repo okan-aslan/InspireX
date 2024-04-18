cbde7fd8eb97bc9baa88b4dee98b3b15d9f0a886
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 21:25:52 +0300

    added user model relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
