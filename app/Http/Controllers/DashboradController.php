aef6f3d4b8b1acba63d170eed6d952aefdf23b16
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 21:24:41 +0300

    added index view for not authenticate users

--- app/Http/Controllers/DashboradController.php
+++ app/Http/Controllers/DashboradController.php
@@ -8,6 +8,10 @@ class DashboradController extends Controller
 {
     public function index()
     {
+        if(auth()->check())
+        {
+            return redirect()->route('tweets.index');
+        }
         return view('welcome');
     }
 }
