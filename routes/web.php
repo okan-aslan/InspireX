cd1a44bd4eda0c87a0c59e43ee564a8811b9a8fd
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 15:43:22 +0300

    added auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboradController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboradController::class, 'index'])->name('dashboard.index');
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+Route::post('/login', [AuthController::class, 'authenticate']);
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+Route::post('/register', [AuthController::class, 'store']);
