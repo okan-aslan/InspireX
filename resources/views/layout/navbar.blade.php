4857b63826ea3674da4407e679ff001cc47237bd
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 15:27:39 +0300

    edited navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-blue-500 p-4">
     <div class="container mx-auto flex justify-between items-center">
         <div>
-            <a href="#" class="text-white font-bold text-lg">Twitter Clone</a>
+            <a href="#" class="text-white font-bold text-lg">{{config('app.name')}}</a>
         </div>
         <div class="flex items-center space-x-4">
             <button class="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full">Login</button>
