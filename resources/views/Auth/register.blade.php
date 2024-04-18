b18350da8a26920e3f0f9732be1c0baf554583e3
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 16:22:50 +0300

    fixed bugs for register-form

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -1,47 +1,57 @@
 @extends('layout.app')
 
 @section('content')
-<form>
-    <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-            Name
-        </label>
-        <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="name" type="text" placeholder="Your Name">
+    @error('name', 'email', 'password', 'password_confirmation')
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+        <strong class="font-bold">Error!</strong>
+        <span class="block sm:inline">{{ $message }}</span>
     </div>
-    <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-            Email Address
-        </label>
-        <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email" type="email" placeholder="Email Address">
-    </div>
-    <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-            Password
-        </label>
-        <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password" type="password" placeholder="Password">
-    </div>
-    <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">
-            Confirm Password
-        </label>
-        <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password_confirmation" type="password" placeholder="Confirm Password">
-    </div>
-    <div class="flex items-center justify-between">
-        <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button">
-            Sign Up
-        </button>
-        <a href="{{route('login')}}" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">Login</a>
-    </div>
-</form>
-
+    @enderror
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                Name
+            </label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name" type="text" placeholder="Your Name" name="name"" required
+                autofocus>
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                Email Address
+            </label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email" type="email" placeholder="Email Address" name="email" required"
+                required>
+        </div>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                Password
+            </label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password" type="password" placeholder="Password" name="password" required>
+        </div>
+        <div class="mb-6">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation" required>
+                Confirm Password
+            </label>
+            <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password_confirmation" type="password" placeholder="Confirm Password" name="password_confirmation"
+                required autocomplete="new-password">
+        </div>
+        <div class="flex items-center justify-between">
+            <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit">
+                Sign Up
+            </button>
+            <a href="{{ route('login') }}"
+                class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">Login</a>
+        </div>
+    </form>
 @endsection
