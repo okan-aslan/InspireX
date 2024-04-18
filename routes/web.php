54b5b0636ef1d342ceac7e9f8bc20934cd9bb5c9
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 16:43:55 +0300

    added logout route

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@ Route::post('/login', [AuthController::class, 'authenticate']);
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/register', [AuthController::class, 'store']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
