2d3e546c0d782abb804a0c67049f079fa07a49e7
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 21:25:31 +0300

    added comment model relations

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tweet_id',
+        'comment_id',
+        'content',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tweet()
+    {
+        return $this->belongsTo(Tweet::class);
+    }
 }
