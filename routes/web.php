31a15fab816473f2db1b6b703ea51a240fb83823
Okan Aslan <aaslan.okan@gmail.com>
2024-04-18 21:27:34 +0300

    added tweet routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboradController;
+use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboradController::class, 'index'])->name('dashboard.index');
@@ -15,3 +16,16 @@ Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'store']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/tweets', [TweetController::class, 'index'])->name('tweets.index');
+
+Route::post('/tweets', [TweetController::class, 'store'])->name('tweets.store');
+
+Route::get('/tweets/{tweet}', [TweetController::class, 'show'])->name('tweets.show');
+
+Route::get('/tweets/{tweet}/edit', [TweetController::class, 'edit'])->name('tweets.edit');
+
+Route::put('/tweets/{tweet}', [TweetController::class, 'update'])->name('tweets.update');
+
+Route::delete('/tweets/{tweet}', [TweetController::class, 'destroy'])->name('tweets.destroy');
+
