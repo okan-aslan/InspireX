ed72e6472f0d52d24e7283e8461aa08e5e16403c
Okan Aslan <aaslan.okan@gmail.com>
2024-04-19 17:22:46 +0300

    added pagination

--- resources/views/Tweet/tweet-index.blade.php
+++ resources/views/Tweet/tweet-index.blade.php
@@ -1,18 +1,19 @@
 @extends('layout.app')
 
 @section('content')
-
     @auth
         @include('Tweet.tweet-create')
     @endauth
 
     @foreach ($tweets as $tweet)
         @include('Tweet.tweet-card')
+        @auth
+            @include('Components.comment-write')
 
-        {{-- @include('Components.comment-write')
-
-        @include('Components.comment') --}}
+            @include('Components.comment')
+        @endauth
     @endforeach
-
-
+    <div class="flex justify-center mt-8">
+        {{$tweets->links()}}
+    </div>
 @endsection
