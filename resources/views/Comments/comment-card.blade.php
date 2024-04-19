6eccb65cd7d5a4ca27a43eee10980ea4887dc07e
Okan Aslan <aaslan.okan@gmail.com>
2024-04-19 23:56:53 +0300

    added @auth & fixed bug

--- resources/views/Comments/comment-card.blade.php
+++ resources/views/Comments/comment-card.blade.php
@@ -6,13 +6,15 @@
                 <div class="font-semibold text-gray-700">{{ $comment->user->name }}</div>
                 <div class="text-gray-500">{{ $comment->updated_at }}</div>
             </div>
-                @if(auth()->user()->id === $comment->user_id)
+            @auth
+                @if (auth()->user()->id === $comment->user_id)
                     <form action="{{ route('comments.destroy', ['comment' => $comment->id]) }}" method="POST" class="ml-auto">
                         @csrf
                         @method('DELETE')
                         <button type="submit" class="text-red-500 hover:text-red-700">Delete</button>
                     </form>
                 @endif
+            @endauth
         </div>
         <p class="text-sm text-gray-700">{{ $comment->content }}</p>
     </div>
