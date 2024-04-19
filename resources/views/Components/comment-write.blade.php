1247fbdc106bb8901f03bccaa5b5bf7ee3bc0eae
Okan Aslan <aaslan.okan@gmail.com>
2024-04-19 21:27:32 +0300

    changed file directory & name

--- resources/views/Components/comment-write.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="mb-8">
-    <div class="comment flex items-center mb-2">
-        <textarea placeholder="Write your comment" class="border rounded-full px-4 py-2 w-full focus:outline-none resize-none"></textarea>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-2 rounded-full ml-2">Post</button>
-    </div>
-</div>
