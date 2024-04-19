cb0619e0eb4c29a61049cc8e750271a874f56b6b
Okan Aslan <aaslan.okan@gmail.com>
2024-04-19 21:26:21 +0300

    added query search funcs

--- resources/views/layout/searchbar.blade.php
+++ resources/views/layout/searchbar.blade.php
@@ -16,15 +16,13 @@
         <!-- End Sample Registered Users -->
 
         <!-- Search Input -->
-        <div class="mt-4">
-            <input type="text" placeholder="Search" class="border rounded-full px-4 py-2 w-full focus:outline-none">
-        </div>
-        <!-- End Search Input -->
-
-        <!-- Search Button -->
-        <div class="mt-4">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full w-full">Search</button>
-        </div>
-        <!-- End Search Button -->
+        <form action="{{route('dashboard.index')}}" method="GET">
+            <div class="mt-4">
+                <input name="search" value="{{request('search', '')}}" type="text" placeholder="Search" class="border rounded-full px-4 py-2 w-full focus:outline-none">
+            </div>
+            <div class="mt-4">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full w-full">Search</button>
+            </div>
+        </form>
     </div>
 </div>
