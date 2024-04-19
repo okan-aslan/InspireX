320f4615f7b1d5bfb6f643b12d7b967de993c8c0
Okan Aslan <aaslan.okan@gmail.com>
2024-04-19 17:03:33 +0300

    added tweet cards to main view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="text-center">
+    <div class="text-center mb-12">
         <h2 class="text-2xl font-semibold mb-4">Welcome to Twitter Clone!</h2>
         <p class="text-lg mb-4">Login or register to start sharing your thoughts with the world.</p>
         <div class="flex justify-center">
@@ -10,5 +10,11 @@
             <a href="{{ route('register') }}"
                 class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-full">Register</a>
         </div>
+    </div>
+    <div>
+        @foreach ($tweets as $tweet)
+            @include('Tweet.tweet-card')
+
+        @endforeach
     </div>
 @endsection
