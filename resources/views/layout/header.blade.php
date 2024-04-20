cd0ccae8397ad87b3b994b2abc6c39c4e8c561d3
Okan Aslan <aaslan.okan@gmail.com>
2024-04-20 14:41:07 +0300

    added material icons cdn

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{config('app.name')}}</title>
+    <title>{{ config('app.name') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
 
 <body class="bg-gray-100">
