4cc7179bcb00f569966a519bdc705664ec73ccdb
Okan Aslan <aaslan.okan@gmail.com>
2024-04-20 14:42:40 +0300

    added dashboard & profile routes

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="w-1/4">
     <div class="bg-white rounded-lg shadow p-4">
         <ul>
-            <li><a href="#" class="block p-4 hover:bg-gray-200">Home</a></li>
+            <li><a href="{{ route('dashboard.index') }}" class="block p-4 hover:bg-gray-200">Home</a></li>
             <li><a href="#" class="block p-4 hover:bg-gray-200">Terms</a></li>
             <li><a href="#" class="block p-4 hover:bg-gray-200">Support</a></li>
-            <li><a href="#" class="block p-4 hover:bg-gray-200">View Profile</a></li>
+            <li><a href="{{ route('profile.index') }}" class="block p-4 hover:bg-gray-200">View Profile</a></li>
         </ul>
     </div>
 </div>
